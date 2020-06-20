b2d548152d354e3c7c4b8f6f336274b34e40be85
Erik Butcher <butcer0@gmail.com>
2020-06-20 14:42:44 -0400

    Initial commit

---
+++ NetworkingAPI/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetworkingAPI.Models;
+
+namespace NetworkingAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ NetworkingAPI/Docs/DotNetCore_3_Startup_Info.md
@@ -0,0 +1,6 @@
+dotnet core 3 setup
+- Nuget: Microsoft.EntityFrameworkCore
+- Nuget: Microsoft.EntityFrameworkCore.Sqlite
+- Terminal: dotnet tool install --global dotnet-ef
+  - Migration Tool
+- 
